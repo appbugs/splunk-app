252a4a6b629bcdc342880dd4da7f686a1821bc5e
seanveri <seany@vericlouds.com>
2016-10-06 22:11:29 +0000

    Fix null pointer error

--- Universal Forwarder Part/bin/forward_creds.ps1
+++ Universal Forwarder Part/bin/forward_creds.ps1
@@ -25,7 +25,7 @@ add-type -TypeDefinition $resultTypeDef;
 
 add-type -AssemblyName "System.Runtime.Serialization"
 
-add-type -path $bcryptNetPath
+#add-type -path $bcryptNetPath
 
 # iterating mailboxes
 $mailboxes=get-mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited;
@@ -42,12 +42,14 @@ $mailboxes | foreach {
 	$account = Get-ADReplAccount -DistinguishedName $dn -server $dc
 	$md5Hash = $null;
 	if ($account -ne $null) {
-		$md5Hash = $account.SupplementalCredentials.WDigest[8]
-		$md5HashString = [string]::Format("{0:x2}{1:x2}{2:x2}{3:x2}{4:x2}{5:x2}{6:x2}{7:x2}{8:x2}{9:x2}{10:x2}{11:x2}{12:x2}{13:x2}{14:x2}{15:x2}",
-		$md5Hash[0], $md5Hash[1], $md5Hash[2], $md5Hash[3], $md5Hash[4], $md5Hash[5], $md5Hash[6], $md5Hash[7],
-		$md5Hash[8], $md5Hash[9], $md5Hash[10], $md5Hash[11], $md5Hash[12], $md5Hash[13], $md5Hash[14], $md5Hash[15]);
-		write-host "email: "$upn ", password-hash: "$md5HashString
+	    if ($account.SupplementalCredentials -ne $null) {
+		    $md5Hash = $account.SupplementalCredentials.WDigest[8]
+		    $md5HashString = [string]::Format("{0:x2}{1:x2}{2:x2}{3:x2}{4:x2}{5:x2}{6:x2}{7:x2}{8:x2}{9:x2}{10:x2}{11:x2}{12:x2}{13:x2}{14:x2}{15:x2}",
+		    $md5Hash[0], $md5Hash[1], $md5Hash[2], $md5Hash[3], $md5Hash[4], $md5Hash[5], $md5Hash[6], $md5Hash[7],
+		    $md5Hash[8], $md5Hash[9], $md5Hash[10], $md5Hash[11], $md5Hash[12], $md5Hash[13], $md5Hash[14], $md5Hash[15]);
+		    write-host "email: "$upn ", password-hash: "$md5HashString
 		
-		Add-Content 'C:\Users\Administrator\Desktop\accounts_credentials.txt' "$upn--|--$md5HashString"
+		    #Add-Content 'C:\Users\Administrator\Desktop\accounts_credentials.txt' "$upn--|--$md5HashString"
+        }
 	}
 }
